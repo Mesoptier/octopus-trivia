39eea0c642da205bbb4b2257234f489a59f87223
Koen Klaren <koen.klaren@gmail.com>
2015-12-17 19:32:02 +0100

    Add basic movement

--- src/states/GameState.js
+++ src/states/GameState.js
@@ -1,15 +1,34 @@
 export default class GameState extends Phaser.State {
 
   create() {
+    const cw = this.game.width;
+    const ch = this.game.height;
+
     this.stage.backgroundColor = '#ffffff';
+    this.world.setBounds(0, 0, 2000, 2000);
+    this.cursors = this.input.keyboard.createCursorKeys();
 
     // Create test object
-    let text = new Phaser.Text(this.game, 250, 250, 'Hello, world!', {
+    this.text = new Phaser.Text(this.game, cw / 2, ch / 2, 'Hello, world!', {
       font: '45px Arial',
       fill: '#ff0000'
     });
-    text.anchor.set(0.5)
-    this.stage.addChild(text);
+    this.text.anchor.set(0.5)
+    this.stage.addChild(this.text);
+  }
+
+  update() {
+    const speed = 5;
+
+    if (this.cursors.left.isDown)
+      this.text.x -= speed;
+    if (this.cursors.right.isDown)
+      this.text.x += speed;
+
+    if (this.cursors.up.isDown)
+      this.text.y -= speed;
+    if (this.cursors.down.isDown)
+      this.text.y += speed;
   }
 
 }
