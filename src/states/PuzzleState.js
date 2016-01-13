b140658a0f133071bf09a18d0f537846e041ba32
Koen Klaren <koen.klaren@gmail.com>
2016-01-13 22:52:24 +0100

    Make puzzle state loadable

--- src/states/PuzzleState.js
+++ src/states/PuzzleState.js
@@ -1,16 +1,25 @@
 import Phaser from 'phaser';
 import Player from '../objects/Player';
+import Dialog from '../helpers/Dialog';
 import renderer from '../renderer';
 
 export default class PuzzleState extends Phaser.State {
 
   create() {
     const { game } = this;
+
+    // Create dialog
+    this.dialog = new Dialog();
+    this.dialog.create(game, (state) => {
+      // Dialog event handler
+    });
+  }
+
+  update() {
+    this.dialog.update();
   }
 
   render() {
-    this.game.dialog.render();
-
     renderer.render();
   }
 
